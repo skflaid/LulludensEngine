9aab031e5bb97759a07e5c20a88443a34f3695e3
Dohyeon Kim <gomshout@gmail.com>
2025-10-02 17:36:07 +0900

    feat: add rendering dependencies

---
+++ Engine/Game.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// Windows header
+#include <Windows.h>
+#include <windowsx.h>
+
+// C/C++ runtime header
+#include <iostream>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include <functional>
+#include <typeinfo>
+
+// DirectX 12
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+
+// #pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
